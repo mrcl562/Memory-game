d8bcdd3335cedb5101012c1861d05b540029a099
Marcelo Llanos Maldonado <mrcl562@gmail.com>
2022-12-08 10:18:14 -0400

    basic stopwatch added

--- Pairs.js
+++ Pairs.js
@@ -1,6 +1,19 @@
 const container = document.querySelector("#card_container");
-const dificulty = document.querySelector("#buttons");
+const dificulty = document.querySelector("#dificulty_buttons");
+const chronometer = document.querySelector("#chronometer");
+var miliseconds_display = document.querySelector(".miliseconds");
+var seconds_display = document.querySelector(".seconds");
+var minutes_display = document.querySelector(".minutes");
+var miliseconds = 0;
+var seconds = 0;
+var minutes = 0;
+var pairs_found = 0;
+
+
+const find_image = "media/find.png";
+const front_image = "media/PlaceHolder.png"
 const cardPair = []
+
 
 //get the images sources
 var images = [];
@@ -12,9 +25,11 @@ for(let i=1; i<=44; i++){
 //first part calls a pair looping as many times needed
 const newElement = (times) =>{
     dificulty.style.display= "none";
+    chronometer.style.display = "grid";
     for(let i=0; i<times; i++)new_pair();
     sortRandom();
-    startInterval();
+    start_pair_interval();
+    start_chronometer();
 }
 
 //second part generates a pair of elements containing an image element hidden
@@ -23,7 +38,8 @@ function new_pair(){
     for(let i=0; i<2; i++){
         container
         .appendChild(Object.assign(document.createElement("div"),{classList: "card"}))
-        .append(Object.assign(document.createElement("img"), {src:images[random], classList: "inner_img card_back"}),Object.assign(document.createElement("img"), {src:"media/PlaceHolder.png", classList:"inner_img card_front"}))
+        .append(Object.assign(document.createElement("img"), {src:images[random], classList: "inner_img card_back"})
+                ,Object.assign(document.createElement("img"), {src:front_image, classList:"inner_img card_front"}))
     }
 }
 //third part sorts the created elements to random spot
@@ -35,32 +51,57 @@ function sortRandom(){
         }
         container.appendChild(frag);
 }
+//stopwatch countdonwn
+const start_chronometer = () =>{
+    chrono = setInterval(() =>{
+        if(seconds > 59){
+            minutes++;
+            seconds = 0;
+        }
+        if(miliseconds > 99){
+            seconds++;
+            miliseconds = 0;
+        }
+        miliseconds++;
+        miliseconds_display.textContent = `${miliseconds}`;
+        seconds_display.textContent = `${seconds}`;
+        minutes_display.textContent = `${minutes}`;
+    },9)
+}
+        
 
-//we add an event listener to the container and inherits to all the elements 
-//It targets the inner image andthe div element
+const stop_chronometer = () =>{
+    clearInterval(chrono);
+    chrono = null;
+}
+
+
+//flip card function 
 container.addEventListener("click", (ev) =>{
     if(ev.target.tagName === "IMG" 
         && cardPair.length < 2){
-            if(!ev.target.parentElement.getElementsByTagName("img")[1].src.match("media/find.png")){
+            if(!ev.target.parentElement.getElementsByTagName("img")[1].src.match(find_image)){
                 ev.target.parentElement.classList.toggle("flipped");
                 cardPair.push(ev.target.parentElement);
             }
     }
 })
 
-//function to evaluate if you found the pairs
-//see if 2 cards has been flip
-
-const startInterval=()=>{
+//see if 2 cards had been flip
+const start_pair_interval=()=>{
     checkTest = setInterval(()=>{
         if (cardPair.length > 1){
+            stop_pair_interval();
             check_pair();
-            stopInterval();
+        }
+        if(pairs_found >= 2){
+            stop_chronometer();
+            stop_pair_interval();
         }
     }, 600);
 }
 
-const stopInterval = () =>{
+const stop_pair_interval = () =>{
     clearInterval(checkTest);
     checkTest = null;
 }
@@ -71,13 +112,14 @@ const check_pair = () =>{
         correct_pair();
         }
     pair_card_flip();
-    startInterval();
+    start_pair_interval();
 }
 
 function correct_pair(){
     cardPair.forEach(card => {
-        card.getElementsByTagName("img")[1].src = "media/find.png";
+        card.getElementsByTagName("img")[1].src = find_image;
     }) 
+    pairs_found++;
 }
 
 function pair_card_flip(){
@@ -86,15 +128,3 @@ function pair_card_flip(){
         cardPair.splice(0, cardPair.length);
     }, 600)
 }
-
-
-
-
-
-
-
-
-
-
-
-
